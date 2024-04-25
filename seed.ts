ac408ed03fd7493d8c08f5d4690a9d45b8a8c190
orzarhi <or.zarhi11@gmail.com>
2024-04-25 12:04:18 +0300

    first commit

---
+++ seed.ts
@@ -0,0 +1,46 @@
+import fs from 'fs';
+import csv from 'csv-parser';
+import { Index } from "@upstash/vector"
+
+interface Row {
+    text: string;
+}
+
+
+const index = new Index({
+    url: process.env.VECTOR_URL,
+    token: process.env.UPSTASH_TOKEN,
+})
+
+async function parseCSV(filePath: string): Promise<Row[]> {
+    return new Promise((resolve, reject) => {
+        const rows: Row[] = [];
+
+        fs.createReadStream(filePath).pipe(csv({ separator: ',' })).on('data', (row) => {
+            rows.push(row);
+        }).on('error', (error) => {
+            reject(error);
+        }).on('end', () => {
+            resolve(rows);
+        });
+    });
+}
+
+const STEP = 30
+const seed = async () => {
+    const data = await parseCSV('training_dataset.csv');
+
+    for (let i = 0; i < data.length; i += STEP) {
+        const chunk = data.slice(i, i + STEP);
+
+        const formatted = chunk.map((row, batchIndex) => ({
+            data: row.text,
+            id: i + batchIndex,
+            metadata: { text: row.text }
+        }))
+
+        await index.upsert(formatted);
+    }
+}
+
+seed();
